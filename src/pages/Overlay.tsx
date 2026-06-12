a03528b8214f7414215871fb6a60a8ca3a528dbe
AxelS27 <farrellaxel2006@gmail.com>
2026-06-13 06:24:04 +0700

    style: reduce overlay alert duration to 3 seconds

--- src/pages/Overlay.tsx
+++ src/pages/Overlay.tsx
@@ -98,7 +98,7 @@ export function Overlay() {
 
       const timer = setTimeout(() => {
         setCurrentDonation(null);
-      }, 10000);
+      }, 3000);
 
       return () => clearTimeout(timer);
     }
